82ff95528af65643d5d2e0552c6505bf0a7d161d
jaehoon166 <jaehoon166.com@gmail.com>
2017-09-29 17:30:45 +0900

    '.'

---
+++ src/main/java/ScannerPlus.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class ScannerPlus{
+    public static void main(String[] arg){
+        Scanner A=new Scanner(System.in);
+        Scanner B=new Scanner(System.in);
+
+        System.out.print(A.nextInt()+B.nextInt());
+    }
+}
